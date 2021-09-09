c7f0ef478d29667ce5900016cc797bc513a9a240
tnsr1 <pepan@mail.ru>
2021-09-09 17:35:31 +0300

    изменение sql

--- ProductCategory.sql
+++ ProductCategory.sql
@@ -1,4 +1,4 @@
-select p.Name [Товар], pc.Name [Категория]
+select p.Name [Товар], c.Name [Категория]
 from Product p
 	left join ProductCategory pc on pc.ProductID = p.ProductID
 	left join Category c on c.CategoryID = pc.CategoryID
