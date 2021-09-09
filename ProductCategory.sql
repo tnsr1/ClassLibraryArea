cdf9978277ddd4e218b5b60a54bce737eca3a97a
tnsr1 <pepan@mail.ru>
2021-09-09 12:47:18 +0300

    Исправление sql

--- ProductCategory.sql
+++ ProductCategory.sql
@@ -1,4 +1,4 @@
-select p.Name [Товар], pc.Name [Категория]
-from [Product] p
-	left join [ProductCategory] pc on pc.[ProductID] = p.ProductSubcategoryID
-	left join [Category] c on c.CategoryID = pc.CategoryID
+select p.Name [РўРѕРІР°СЂ], pc.Name [РљР°С‚РµРіРѕСЂРёСЏ]
+from Product p
+	left join ProductCategory pc on pc.ProductID = p.ProductID
+	left join Category c on c.CategoryID = pc.CategoryID
